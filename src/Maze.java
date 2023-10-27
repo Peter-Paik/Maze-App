944612c8f7693ef79388d3bf8f572c526ae06e7f
Peter-Paik <pjpaik@stu.naperville203.org>
2023-10-27 13:54:11 -0500

    Maze skeleton

---
+++ src/Maze.java
@@ -0,0 +1,29 @@
+public class Maze {
+    public Maze(){
+
+    }
+
+    public boolean loadMaze(String fname){
+
+    }
+
+    public ArrayList<Square> getNeighbors(Square sq){
+
+    }
+
+    public Square getStart(){
+
+    }
+
+    public Square getFinish(){
+
+    }
+
+    public void reset(){
+
+    }
+
+    public String toString(){
+        
+    }
+}
