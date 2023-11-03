040082d1f57361a8f6c00d125989273f8475495a
Peter-Paik <pjpaik@stu.naperville203.org>
2023-11-03 14:14:50 -0500

    started mazesolverqueue

---
+++ src/MazeSolverQueue.java
@@ -0,0 +1,20 @@
+public class MazeSolverQueue extends MazeSolver {
+    
+    public MazeSolverQueue(){
+        super();
+    }
+
+    public void makeEmpty(){
+
+    }
+    public boolean isEmpty(){
+        return true;//change later
+    }
+    public void add(Square sq){
+        
+    }
+    public Square next(){
+        return new Square(1,1,1); //change later
+    }
+
+}
